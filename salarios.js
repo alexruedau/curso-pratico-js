c93f2f99f3b0790ac7904617d237871f54f4a276
alexruedau <alexruedau@gmail.com>
2021-08-08 03:02:01 +0200

    Taller de salarios

---
+++ salarios.js
@@ -0,0 +1,53 @@
+const colombia = [];
+colombia.push({
+    name: "Juanita", 
+    salary: 1000,
+});
+colombia.push({
+    name: "Natalia", 
+    salary: 900,
+});
+colombia.push({
+    name: "Pedrito", 
+    salary: 500,
+});
+colombia.push({
+    name: "Oscar", 
+    salary: 5000,
+});
+colombia.push({
+    name: "Luis", 
+    salary: 100,
+});
+colombia.push({
+    name: "Leonie", 
+    salary: 900,
+});
+colombia.push({
+    name: "Carolina", 
+    salary: 2000,
+});
+colombia.push({
+    name: "Alejandra", 
+    salary: 1500,
+});
+colombia.push({
+    name: "Marje", 
+    salary: 450,
+});
+colombia.push({
+    name: "Anna", 
+    salary: 3000,
+});
+colombia.push({
+    name: "Carlos", 
+    salary: 2500,
+});
+colombia.push({
+    name: "Alex", 
+    salary: 2500000,
+});
+colombia.push({
+    name: "Alejandro", 
+    salary: 25000000,
+});
